75957197115540cb3c004d0cf7a3cffd6d7a21bf
zadnap <zadnap@users.noreply.github.com>
2024-08-31 21:59:20 +0700

    Combine the logic with the UI
    Now the player can tab on one of the 9 spots to
    play. By default, the player with 'x' mark starts
    first. After the game is finished (tie or not),
    a dialog modal show up and inform the result with
    a button to start a new game. They can also start
    a new game by clicking the reset button below the
    game board.

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const Gameboard = (function () {
 
   function updateBoard(index, marker) {
     board[index] = marker;
+    DisplayController.updateTile(index);
   }
 
   return {
@@ -41,6 +42,7 @@ const GameController = (function () {
   function addPlayers(player1, player2) {
     players = [player1, player2];
   }
+  addPlayers(Player("x"), Player("o"));
 
   function checkWin() {
     const board = Gameboard.getBoard();
@@ -72,26 +74,37 @@ const GameController = (function () {
     if (!gameOver && !Gameboard.getBoard()[index]) {
       Gameboard.updateBoard(index, players[currentPlayerIndex].marker);
       if (checkWin()) {
+        const resultDialog = document.querySelector(".result-dialog");
+        resultDialog.classList.add("show");
+        resultDialog.showModal();
+        resultDialog.querySelector(
+          "h3"
+        ).textContent = `[${players[currentPlayerIndex].marker}] wins!`;
         gameOver = true;
-        console.log(`${players[currentPlayerIndex].name} win!`);
       } else if (checkTie()) {
+        const resultDialog = document.querySelector(".result-dialog");
+        resultDialog.classList.add("show");
+        resultDialog.showModal();
+        resultDialog.querySelector("h3").textContent = "It's a tie";
         gameOver = true;
-        console.log("It's a tie");
       } else {
         // Switch turn
         currentPlayerIndex = 1 - currentPlayerIndex;
+        DisplayController.updateCurrentPlayer(
+          players[currentPlayerIndex].marker
+        );
       }
     }
   }
 
   function resetGame() {
-    Gameboard.resetBoard();
     currentPlayerIndex = 0;
     gameOver = false;
+    Gameboard.resetBoard();
+    DisplayController.renderBoard();
   }
 
   return {
-    addPlayers,
     playTurn,
     resetGame,
   };
@@ -101,13 +114,34 @@ const DisplayController = (function () {
   function renderBoard() {
     const gameboard = document.querySelector(".gameboard");
     gameboard.innerHTML = "";
-    Gameboard.getBoard().forEach((tile) => {
+    Gameboard.getBoard().forEach(() => {
       const newTile = document.createElement("div");
       newTile.classList.add("tile");
+      gameboard.appendChild(newTile);
+    });
+  }
+  renderBoard();
 
-      switch (tile) {
-        case "x":
-          newTile.innerHTML = `<svg
+  function updateCurrentPlayer(marker) {
+    const container = document.querySelector(".container");
+    const header = document.querySelector("header h3");
+    header.textContent = `${marker}'s turn`;
+
+    if (marker === "o") {
+      container.classList.remove("x-turn");
+    } else if (marker === "x") {
+      container.classList.remove("o-turn");
+    }
+
+    container.classList.add(`${marker}-turn`);
+  }
+
+  function updateTile(index) {
+    const tiles = document.querySelectorAll(".tile");
+
+    switch (Gameboard.getBoard()[index]) {
+      case "x":
+        tiles[index].innerHTML = `<svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 384 512"
             fill="currentColor"
@@ -117,10 +151,10 @@ const DisplayController = (function () {
               d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
             />
           </svg>`;
-          newTile.classList.add("x");
-          break;
-        case "o":
-          newTile.innerHTML = `<svg
+        tiles[index].classList.add("x");
+        break;
+      case "o":
+        tiles[index].innerHTML = `<svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 448 512"
             fill="currentColor"
@@ -130,19 +164,47 @@ const DisplayController = (function () {
               d="M224 96a160 160 0 1 0 0 320 160 160 0 1 0 0-320zM448 256A224 224 0 1 1 0 256a224 224 0 1 1 448 0z"
             />
           </svg>`;
-          newTile.classList.add("o");
-          break;
-        default:
-          break;
-      }
+        tiles[index].classList.add("o");
+        break;
+      default:
+        break;
+    }
+  }
 
-      gameboard.appendChild(newTile);
+  function listenChoosingTile() {
+    const tiles = Array.from(document.querySelectorAll(".tile"));
+    const gameboard = document.querySelector(".gameboard");
+
+    gameboard.addEventListener("click", (e) => {
+      const tileIndex = tiles.indexOf(e.target);
+
+      // If the tile is already clicked once, do not allow to click it one more time
+      if (e.target !== gameboard && tileIndex !== -1) {
+        GameController.playTurn(tileIndex);
+      }
     });
   }
+  listenChoosingTile();
+
+  function listenReseting() {
+    const reset = document.querySelector(".reset");
+    const startNew = document.querySelector(".start-new");
+
+    const handleResetGame = () => {
+      const resultDialog = document.querySelector(".result-dialog");
+      resultDialog.classList.remove("show");
+      resultDialog.close();
+      GameController.resetGame();
+    };
+
+    reset.addEventListener("click", handleResetGame);
+    startNew.addEventListener("click", handleResetGame);
+  }
+  listenReseting();
 
   return {
     renderBoard,
+    updateTile,
+    updateCurrentPlayer,
   };
 })();
-
-DisplayController.renderBoard();
